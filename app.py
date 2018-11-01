9fd47936d58b32d9f21539065847577fd757da02
Bryan <bryches@gmail.com>
2018-11-01 17:31:05 -0400

    Added select sprint feature. Added profile template.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime, timedelta
-from random import randint
-from passlib import pwd, hash
+from random   import randint
+from passlib  import pwd, hash
 
 app = Flask(__name__)
 app.debug = True
@@ -17,44 +18,44 @@ db = SQLAlchemy(app)
 
 
 class Sprint(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    desc = db.Column(db.String(500))
+    id    = db.Column(db.Integer, primary_key=True)
+    name  = db.Column(db.String(50))
+    desc  = db.Column(db.String(500))
     start = db.Column(db.DateTime)
-    end = db.Column(db.DateTime)
+    end   = db.Column(db.DateTime)
     items = db.relationship('Item', backref='sprint', lazy=True)
 
 
-
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(50))
+    id        = db.Column(db.Integer, primary_key=True)
+    status    = db.Column(db.String(50))
     sprint_id = db.Column(db.Integer, db.ForeignKey('sprint.id'))
+    name      = db.Column(db.String(50))
+    desc      = db.Column(db.String(500))
+    created   = db.Column(db.DateTime)
+    created_by_id  = db.Column(db.Integer, db.ForeignKey('user.id'))
     assigned_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(50))
-    desc = db.Column(db.String(500))
-    created = db.Column(db.DateTime)
-    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id      = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
-    txt = db.Column(db.String(500))
+    txt     = db.Column(db.String(500))
     created = db.Column(db.DateTime)
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    id       = db.Column(db.Integer, primary_key=True)
+    name     = db.Column(db.String(50))
     username = db.Column(db.String(50))
     password = db.Column(db.String(100))
     temp_pw  = db.Column(db.String(20))
-    created_items = db.relationship('Item', backref='assigned_to', lazy=True, foreign_keys='Item.assigned_to_id')
-    assigned_items = db.relationship('Item', backref='created_by', lazy=True, foreign_keys='Item.created_by_id')
-    comments = db.relationship('Comment', backref='created_by', lazy=True, foreign_keys='Comment.created_by_id')
+    comments       = db.relationship('Comment', backref='created_by',  lazy=True, foreign_keys='Comment.created_by_id')
+    created_items  = db.relationship('Item',    backref='assigned_to', lazy=True, foreign_keys='Item.assigned_to_id')
+    assigned_items = db.relationship('Item',    backref='created_by',  lazy=True, foreign_keys='Item.created_by_id')
+    selected_sprint_id = db.Column(db.Integer, db.ForeignKey('sprint.id'))
+
 
 
 
@@ -62,6 +63,9 @@ class User(db.Model):
 @app.route('/')
 def home():
     return redirect('/sprints/current')
+
+
+
 
 
 @app.before_request
@@ -83,40 +87,39 @@ def backlog():
 
 
 @app.route('/backlog/new', methods=['GET', 'POST'])
-def new_item_backlog():
+def new_item():
     if 'submit' in request.form:
         name = request.form['name']
         desc = request.form['desc'] if request.form['desc'] else None
-        created_by_id = session['user']['id'] if 'user' in session else None
-        assigned_to_id = int(request.form['user']) if 'user' in request.form and request.form['user'] else None
-        sprint_id = int(request.form['sprint']) if 'sprint' in request.form and request.form['sprint'] else None
+        created_by_id  = session['user']['id']       if 'user'   in session else None
+        assigned_to_id = int(request.form['user'])   if 'user'   in request.form and request.form['user']   else None
+        sprint_id      = int(request.form['sprint']) if 'sprint' in request.form and request.form['sprint'] else None
 
         if name:
-            item = Item(name=name, status='Pending', sprint_id=sprint_id, created=datetime.utcnow(), created_by_id=created_by_id, assigned_to_id=assigned_to_id, desc=desc)
+            item = Item(
+                name=name,
+                status='Pending',
+                sprint_id=sprint_id,
+                created=datetime.utcnow(),
+                created_by_id=created_by_id,
+                assigned_to_id=assigned_to_id,
+                desc=desc
+            )
             db.session.add(item)
             db.session.commit()
             return redirect('/backlog')
+
     return render_template('edit-item.html', title='New Item', sprints=Sprint.query.all(), users=User.query.all())
+
+
+
 
 
 @app.route('/sprints/current')
 def current():
-	sprint = Sprint.query.filter(Sprint.start <= datetime.now()).order_by(Sprint.start.desc()).first()
-
-	if sprint:
-		return redirect('/sprints/' + str(sprint.id))
-	else:
-		return redirect('/sprints' + id)
-
-
-
-
-@app.route('/sprints/next')
-def next():
-    sprints = Sprint.query.filter(Sprint.end > datetime.utcnow()).order_by(Sprint.end).limit(2).all()
-    sprint = sprints[0] if sprints[0].start > datetime.utcnow() else sprints[1]
-    return redirect('/sprints/' + str(sprint.id)) if sprint else redirect('/sprints')
-
+    sprint = Sprint.query.filter(Sprint.start <= datetime.now()).order_by(Sprint.start.desc()).first()
+    url    = '/sprints/' + str(sprint.id) if sprint else '/sprints' + id
+    return redirect(url)
 
 
 
@@ -124,25 +127,49 @@ def next():
 
 @app.route('/sprints')
 def sprints():
-	sprints = Sprint.query.order_by(Sprint.start.desc()).all()
-	print(sprints)
-	return render_template('sprints.html', title='Sprints', sprints=sprints)
+    sprints = Sprint.query.order_by(Sprint.start.desc()).all()
+    return render_template('sprints.html', title='Sprints', sprints=sprints)
+
+
+
+
 
 STATUS_COLORS = {'Pending': 'secondary', 'In Process': 'primary', 'Testing': 'warning', 'Complete': 'success'}
-STATUS_LIST = ['Pending', 'In Process', 'Testing', 'Complete']
+STATUS_LIST   = ['Pending', 'In Process', 'Testing', 'Complete']
 
 @app.route('/sprints/<int:id>')
 def sprint(id):
     sprint = Sprint.query.get(id)
-    items = Item.query.filter_by(sprint_id=id).all()
+    items  = Item.query.filter_by(sprint_id=id).all()
     days_left = (sprint.end - datetime.utcnow()).days
     
     data = []
     for status in STATUS_LIST:
         filtered = [item for item in items if item.status == status]
-        data.append({ 'status': status, 'color': STATUS_COLORS[status], 'count': len(filtered), 'items': filtered })
+
+        data.append({
+            'status': status,
+            'color':  STATUS_COLORS[status],
+            'count':  len(filtered),
+            'items':  filtered
+        })
 
     return render_template('sprint.html', title=sprint.name, data=data, sprint=sprint, days_left=days_left)
+
+
+
+
+
+@app.route('/sprints/<int:id>/select')
+def select_sprint(id):
+    sprint = Sprint.query.get(id)
+    user   = User.query.get(session['user']['id'])
+
+    session['sprint'] = { 'id': id, 'name': sprint.name }
+    user.selected_sprint_id = id
+
+    db.session.commit()
+    return redirect('/sprints')
 
 
 
@@ -161,6 +188,7 @@ def new_sprint():
         if name and start and end:
             if end > start and not overlap(start, end):
                 sprint = Sprint(name=name, start=start, end=end, desc=desc)
+
                 db.session.add(sprint)
                 db.session.commit()
                 return redirect('/sprints')
@@ -174,14 +202,14 @@ def new_sprint():
     return render_template('edit-sprint.html', title='New Sprint', default_start=default_start, default_end=default_end)
 
 
-
-
-
 def overlap(start, end):
     before = Sprint.query.filter(Sprint.end < start).count()
     after  = Sprint.query.filter(Sprint.start > end).count()
     total  = Sprint.query.count()
+
     return total - before - after
+
+
 
 
 
@@ -201,6 +229,7 @@ def edit_sprint(id):
                 sprint.start = start
                 sprint.end   = end
                 sprint.desc  = desc
+
                 db.session.commit()
                 return redirect('/sprints')
 
@@ -225,12 +254,6 @@ def delete_sprint(id):
 
 
 
-
-
-
-
-
-
 @app.route('/items/<int:id>', methods=['GET', 'POST'])
 def item(id):
     item = Item.query.get(id)
@@ -244,21 +267,22 @@ def item(id):
             db.session.commit()
 
     comments = Comment.query.filter_by(item_id=id).order_by(Comment.created)
+
     return render_template('item.html', item=item, comments=comments, status_colors=STATUS_COLORS)
+
 
 
 
 
 @app.route('/items/<int:id>/activate')
 def activate_item(id):
-    sprints = Sprint.query.filter(Sprint.end > datetime.utcnow()).order_by(Sprint.end).limit(2).all()
-    if sprints:
-        if len(sprints) == 1: sprint = sprints[0]
-        else: sprint = sprints[0] if sprints[0].start > datetime.utcnow() else sprints[1]
+    if 'sprint' in session:
         item = Item.query.get(id)
-        item.sprint_id = sprint.id
+        item.sprint_id = session['sprint']['id']
         db.session.commit()
+
     return redirect('/backlog')
+
 
 
 
@@ -302,12 +326,13 @@ def delete_item(id):
 
 
 
+
 @app.route('/items/<int:id>/demote')
 def demote_item(id):
-    item = Item.query.get(id)
+    item      = Item.query.get(id)
     sprint_id = item.sprint_id
-    index = STATUS_LIST.index(item.status) if item.status in STATUS_LIST else None
-    
+    index     = STATUS_LIST.index(item.status) if item.status in STATUS_LIST else None
+
     if index == None:
         item.status = STATUS_LIST[0]
         item.sprint_id = None
@@ -320,31 +345,39 @@ def demote_item(id):
 
 
 
+
 @app.route('/items/<int:id>/promote')
 def promote_item(id):
-    item = Item.query.get(id)
+    item      = Item.query.get(id)
     sprint_id = item.sprint_id
-    index = STATUS_LIST.index(item.status) if item.status in STATUS_LIST else None
-    
+    index     = STATUS_LIST.index(item.status) if item.status in STATUS_LIST else None
+
     if index == None:
         item.status = STATUS_LIST[0]
         item.sprint_id = None
     else:
         item.status = STATUS_LIST[index + 1] if index < len(STATUS_LIST) - 1 else STATUS_LIST[len(STATUS_LIST) - 1]
-        print(item.status)
 
     db.session.commit()
     return redirect('/sprints/' + str(sprint_id))
+
+
+
 
 
 @app.route('/items/<int:id>/backlog')
 def backlog_item(id):
-    item = Item.query.get(id)
-    sprint_id = item.sprint_id
+    item        = Item.query.get(id)
+    sprint_id   = item.sprint_id
+
     item.status = STATUS_LIST[0]
     item.sprint_id = None
+
     db.session.commit()
     return redirect('/sprints/' + str(sprint_id))
+
+
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -360,15 +393,16 @@ def login():
                 if user.temp_pw:
                     if True: #password == user.temp_pw:
                         session['user'] = { 'id': user.id, 'name': user.name, 'username': user.username }
-                        print('Logged in')
                         return redirect('/')
+
                 elif user.password:
                     if hash.pbkdf2_sha256.verify(password, user.password):
                         session['user'] = { 'id': user.id, 'name': user.name, 'username': user.username }
                         return redirect('/')
-    
+
     if 'user' in session: session.pop('user')
     return render_template('login.html', title='Login')
+
 
 
 
@@ -377,6 +411,16 @@ def login():
 def logout():
     session.clear()
     return redirect('/login')
+
+
+
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', title=session['user']['name'])
+
+
 
 
 
@@ -391,7 +435,8 @@ def new_user():
             duplicate = User.query.filter_by(username=username).count()
             
             if not duplicate:
-                user = User(name=name, username=username, temp_pw=temp_pw, password=None)
+                user = User(name=name, username=username, temp_pw=temp_pw, password=None, selected_sprint_id=None)
+
                 db.session.add(user)
                 db.session.commit()
                 return redirect('/users')
@@ -416,8 +461,6 @@ def reset_password(id):
 
 
 
-
-
 @app.route('/users/<int:id>/edit', methods=['GET', 'POST'])
 def edit_user(id):
     user = User.query.get(id)
@@ -432,11 +475,11 @@ def edit_user(id):
             if not duplicate:
                 user.name     = name
                 user.username = username
+
                 db.session.commit()
                 return redirect('/users')
 
     return render_template('edit-user.html', title='Edit User', user=user)
-
 
 
 
@@ -448,16 +491,21 @@ def users():
     return render_template('users.html', title='Users', users=users)
 
 
+
+
+
 @app.route('/users/<int:id>/delete', methods=['GET', 'POST'])
 def delete_user(id):
     user = User.query.get(id)
-    
+
     if 'submit' in request.form:
         db.session.delete(user)
         db.session.commit()
         return redirect('/users')
 
     return render_template('delete.html', title=user.name, type='User', cancel='/users')
+
+
 
 
 
